1b0557803000cdca8a095d381f808dd09f7db544
Gms-Tech <107341515+Gms-Tech@users.noreply.github.com>
2023-07-31 13:25:19 +0300

    Add project files.

---
+++ splitbar/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace splitbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.toQutTextBox = new System.Windows.Forms.TextBox();
+            this.fromQutTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.fromSKUTextBox = new System.Windows.Forms.TextBox();
+            this.toSKUTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.fromDateTextBox = new System.Windows.Forms.TextBox();
+            this.toDateTextBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.fromStocknumTextBox = new System.Windows.Forms.TextBox();
+            this.toStocknumTextBox = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.codeTextBox = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(2, 1);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 439);
+            this.checkedListBox1.TabIndex = 0;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // toQutTextBox
+            // 
+            this.toQutTextBox.Location = new System.Drawing.Point(270, 44);
+            this.toQutTextBox.Name = "toQutTextBox";
+            this.toQutTextBox.Size = new System.Drawing.Size(100, 20);
+            this.toQutTextBox.TabIndex = 2;
+            // 
+            // fromQutTextBox
+            // 
+            this.fromQutTextBox.Location = new System.Drawing.Point(414, 45);
+            this.fromQutTextBox.Name = "fromQutTextBox";
+            this.fromQutTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fromQutTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(565, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "משקל";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(376, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "עד";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(520, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "מ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(520, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "מ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(376, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "עד";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label6.Location = new System.Drawing.Point(565, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "מק\"ט";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // fromSKUTextBox
+            // 
+            this.fromSKUTextBox.Location = new System.Drawing.Point(414, 103);
+            this.fromSKUTextBox.Name = "fromSKUTextBox";
+            this.fromSKUTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fromSKUTextBox.TabIndex = 10;
+            // 
+            // toSKUTextBox
+            // 
+            this.toSKUTextBox.Location = new System.Drawing.Point(270, 102);
+            this.toSKUTextBox.Name = "toSKUTextBox";
+            this.toSKUTextBox.Size = new System.Drawing.Size(100, 20);
+            this.toSKUTextBox.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(520, 171);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "מ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(376, 167);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(21, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "עד";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label9.Location = new System.Drawing.Point(565, 161);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(52, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "תאריך";
+            // 
+            // fromDateTextBox
+            // 
+            this.fromDateTextBox.Location = new System.Drawing.Point(414, 161);
+            this.fromDateTextBox.Name = "fromDateTextBox";
+            this.fromDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fromDateTextBox.TabIndex = 16;
+            // 
+            // toDateTextBox
+            // 
+            this.toDateTextBox.Location = new System.Drawing.Point(270, 160);
+            this.toDateTextBox.Name = "toDateTextBox";
+            this.toDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.toDateTextBox.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(520, 226);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(14, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "מ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(376, 222);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 13);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "עד";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label12.Location = new System.Drawing.Point(565, 222);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(83, 20);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "מספר מלאי";
+            // 
+            // fromStocknumTextBox
+            // 
+            this.fromStocknumTextBox.Location = new System.Drawing.Point(414, 216);
+            this.fromStocknumTextBox.Name = "fromStocknumTextBox";
+            this.fromStocknumTextBox.Size = new System.Drawing.Size(100, 20);
+            this.fromStocknumTextBox.TabIndex = 22;
+            // 
+            // toStocknumTextBox
+            // 
+            this.toStocknumTextBox.Location = new System.Drawing.Point(270, 215);
+            this.toStocknumTextBox.Name = "toStocknumTextBox";
+            this.toStocknumTextBox.Size = new System.Drawing.Size(100, 20);
+            this.toStocknumTextBox.TabIndex = 21;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(158, 213);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "פצל";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(239, 218);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 26;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // codeTextBox
+            // 
+            this.codeTextBox.Location = new System.Drawing.Point(318, 301);
+            this.codeTextBox.Name = "codeTextBox";
+            this.codeTextBox.Size = new System.Drawing.Size(196, 20);
+            this.codeTextBox.TabIndex = 27;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label13.Location = new System.Drawing.Point(552, 301);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(34, 20);
+            this.label13.TabIndex = 28;
+            this.label13.Text = "קןד";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.codeTextBox);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.fromStocknumTextBox);
+            this.Controls.Add(this.toStocknumTextBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.fromDateTextBox);
+            this.Controls.Add(this.toDateTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.fromSKUTextBox);
+            this.Controls.Add(this.toSKUTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fromQutTextBox);
+            this.Controls.Add(this.toQutTextBox);
+            this.Controls.Add(this.checkedListBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.TextBox toQutTextBox;
+        private System.Windows.Forms.TextBox fromQutTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox fromSKUTextBox;
+        private System.Windows.Forms.TextBox toSKUTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox fromDateTextBox;
+        private System.Windows.Forms.TextBox toDateTextBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox fromStocknumTextBox;
+        private System.Windows.Forms.TextBox toStocknumTextBox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox codeTextBox;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ splitbar/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace splitbar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // קבלת הערכים מה-TextBoxes והצ'קבוקס
+            string code = codeTextBox.Text;
+            int fromQut = int.Parse(fromQutTextBox.Text);
+            int toQut = int.Parse(toQutTextBox.Text);
+            int fromSKU = int.Parse(fromSKUTextBox.Text);
+            int toSKU = int.Parse(toSKUTextBox.Text);
+            int fromDate = int.Parse(fromDateTextBox.Text);
+            int toDate = int.Parse(toDateTextBox.Text);
+            int fromStocknum = int.Parse(fromStocknumTextBox.Text);
+            int toStocknum = int.Parse(toStocknumTextBox.Text);
+            bool ignoreStocknum = checkBox1.Checked;
+
+            // בניית מערך שמכיל את התווים מהקוד
+            string[] codeArray = new string[]
+            {
+        GetSubstring(code, fromQut, toQut),          // Qut
+        GetSubstring(code, fromSKU, toSKU),          // SKU
+        GetSubstring(code, fromDate, toDate),        // Date
+        GetSubstring(code, fromStocknum, toStocknum) // Stock
+            };
+             string GetSubstring(string input, int startIndex, int length)
+            {
+                if (startIndex >= 0 && length > 0 && input.Length >= startIndex + length)
+                {
+                    return input.Substring(startIndex, length);
+                }
+                return string.Empty;
+            }
+
+
+
+
+            // בניית שאילתת החיפוש בהתאם לפרמטרים שהתקבלו
+            string sqlQuery = "SELECT * FROM YourTable WHERE Code = @code AND Quantity BETWEEN @fromQut AND @toQut " +
+                              "AND SKU BETWEEN @fromSKU AND @toSKU AND Date BETWEEN @fromDate AND @toDate ";
+
+            if (!ignoreStocknum)
+            {
+                sqlQuery += "AND StockNum BETWEEN @fromStocknum AND @toStocknum ";
+            }
+
+            // כמובן שכדאי להתאים את שמות העמודות והטבלה לבסיס הנתונים שלך
+
+            using (SqlConnection connection = new SqlConnection("Your_Connection_String"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    // הגדרת הפרמטרים של השאילתה
+                    command.Parameters.AddWithValue("@code", code);
+                    command.Parameters.AddWithValue("@fromQut", fromQut);
+                    command.Parameters.AddWithValue("@toQut", toQut);
+                    command.Parameters.AddWithValue("@fromSKU", fromSKU);
+                    command.Parameters.AddWithValue("@toSKU", toSKU);
+                    command.Parameters.AddWithValue("@fromDate", fromDate);
+                    command.Parameters.AddWithValue("@toDate", toDate);
+
+                    if (!ignoreStocknum)
+                    {
+                        command.Parameters.AddWithValue("@fromStocknum", fromStocknum);
+                        command.Parameters.AddWithValue("@toStocknum", toStocknum);
+                    }
+
+                    // ביצוע השאילתה ועיבוד התוצאות
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // כאן תוכל לעשות משהו עם התוצאות, לדוגמה להציגם בטבלה או ברשימת בחירה
+                    // dataGridView1.DataSource = dataTable;
+                    // listBox1.DataSource = dataTable;
+                    // וכדומה...
+                }
+            }
+        }
+
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string tableName = "שם_הטבלה"; // השלם כאן את שם הטבלה בבסיס הנתונים שלך
+
+            string connectionString = "Data Source=שם_שרת;Initial Catalog=שם_בסיס_הנתונים;Integrated Security=True"; // השלם כאן את פרטי החיבור לבסיס הנתונים
+
+            string sql = string.Format("SELECT * FROM {0}", tableName);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    dataAdapter.Fill(dataTable);
+
+                    // כאן תוכל לעשות משהו עם הנתונים, לדוגמה להציגם בטבלה או ברשימת בחירה (CheckedListBox)
+                    // checkedListBox1.DataSource = dataTable;
+                    // checkedListBox1.DisplayMember = "שם העמודה להצגה";
+                    // וכדומה...
+                }
+                catch (Exception ex)
+                {
+                    // טיפול בשגיאות במידה ויש
+                    MessageBox.Show("שגיאה בעת טעינת הנתונים: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
